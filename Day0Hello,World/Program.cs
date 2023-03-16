39a6b51ae0b20da5f7e8dcf79d5c3d41d3b4c437
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-03-17 00:15:10 +0300

    HackerRank Day 0: Hello,World problemi çözüldü

---
+++ Day0Hello,World/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day0Hello_World
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString;
+            inputString = Console.ReadLine();
+            Console.WriteLine("Hello, World.\n{0}", inputString);
+        }
+    }
+}
