c4aff4aee790b16f11dd60f7d4499f78ee9797f0
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-03-18 02:49:08 +0300

    HackerRank Day 7: Arrays problemi çözüldü

---
+++ Day7Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7Arrays
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+            string array = string.Empty;
+            foreach (int i in arr)
+            {
+                array = i.ToString() + " " + array;
+            }
+
+            Console.WriteLine(array);
+        }
+    }
+}
