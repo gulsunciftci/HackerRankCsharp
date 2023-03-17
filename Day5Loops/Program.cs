a5fe2fffc51cb05d4e56cb5b23c2399417650ced
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-03-18 02:10:32 +0300

    HackerRank Day 5: Loops problemi çözüldü

---
+++ Day5Loops/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5Loops
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+            if (2 <= n && n <= 20)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+
+                    Console.WriteLine("{0} x {1} = {2}", n, i, i * n);
+                }
+            }
+        }
+    }
+ }
+
