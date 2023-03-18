55b1ecc94494dcd459c77975c5ea95d093417023
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-03-18 23:52:06 +0300

    HackerRank Day 16: Exceptions - String to Integer, Day 17: More Exceptions  problemleri çözüldü

---
+++ Day16ExceptionsStringtoInteger/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16ExceptionsStringtoInteger
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine(int.Parse(S));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Bad String");
+            }
+        }
+    }
+}
---
+++ Day17MoreExceptions/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day17MoreExceptions
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator myCalculator = new Calculator();
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                string[] num = Console.ReadLine().Split();
+                int n = int.Parse(num[0]);
+                int p = int.Parse(num[1]);
+                try
+                {
+                    int ans = myCalculator.power(n, p);
+                    Console.WriteLine(ans);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+
+                }
+            }
+        }
+    }
+    public class Calculator
+    {
+
+        public int power(int n, int p)
+        {
+            if (n < 0 || p < 0)
+            {
+                throw new Exception("n and p should be non-negative");
+            }
+            else
+            {
+                return Convert.ToInt32(Math.Pow(n, p));
+            }
+        }
+    }
+}
