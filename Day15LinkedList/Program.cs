0fdc9602cea7a3292d6d036c45058731ebb1fa8c
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-03-18 22:48:26 +0300

    HackerRank Day 15: Linked List problemi çözüldü

---
+++ Day15LinkedList/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15LinkedList
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Node head = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                head = insert(head, data);
+            }
+            display(head);
+        }
+
+        private static void display(Node head)
+        {
+            Node start = head;
+            while (start != null)
+            {
+                Console.Write(start.data + " ");
+                start = start.next;
+            }
+        }
+
+        private static Node insert(Node head, int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                if (head.next == null)
+                {
+                    head.next = node;
+                }
+                else
+                {
+                    Node current = head;
+                    while (current.next != null)
+                    {
+                        current = current.next;
+                    }
+                    current.next = node;
+                }
+            }
+            return head;
+        }
+    }
+    class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+
+    }
+}
