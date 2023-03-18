f5a5a0791cffdbc7ea79e3eea8d6db8a792b40c4
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-03-18 05:46:39 +0300

    HackerRank Day 9: Recursion 3 problemi çözüldü

---
+++ Day9Recursion3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9Recursion3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = File.CreateText("d:\\Recursion.txt");
+           
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+            int result = 0;
+            if (n >= 2 && n <= 12)
+            {
+                result = Result.factorial(n);
+            }
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+    class Result
+    {
+
+        /*
+         * Complete the 'factorial' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts INTEGER n as parameter.
+         */
+
+        public static int factorial(int n)
+        {
+
+            if (n >= 2)
+            {
+                return n * factorial(n - 1);
+            }
+            return 1;
+        }
+
+    }
+
+}
