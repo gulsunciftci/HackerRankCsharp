8d70d760a13d3e2a05c1d8bfdbdc7bc837ca309e
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-03-18 21:09:58 +0300

    HackerRank Day 14: Scope problemi çözüldü

---
+++ Day14Scope/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14Scope
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Convert.ToInt32(Console.ReadLine());
+
+            int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+            Difference d = new Difference(a);
+
+            d.computeDifference();
+
+            Console.Write(d.maximumDifference);
+        }
+    }
+    class Difference
+    {
+        private int[] elements;
+        public int maximumDifference;
+
+        public Difference(int[] _a)
+        {
+            elements = _a;
+        }
+
+        public void computeDifference()
+        {
+            maximumDifference = -1000000;
+            for (int j = 0; j < elements.Length - 1; j++)
+            {
+                for (int i = j + 1; i < elements.Length; i++)
+                {
+                    int difference = Math.Abs(elements[j] - elements[i]);
+                    if (difference > maximumDifference)
+                    {
+                        maximumDifference = difference;
+                    }
+                }
+            }
+        }
+    }
+}
