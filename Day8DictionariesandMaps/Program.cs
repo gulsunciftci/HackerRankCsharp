9a9dd2c9b813339a1469eda9754c7e5c16af3e8b
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-03-18 03:22:23 +0300

    HackerRank Day 8: Dictionaries and Maps problemi çözüldü

---
+++ Day8DictionariesandMaps/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8DictionariesandMaps
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> nameNum = new Dictionary<string, string>();
+            int n = int.Parse(Console.ReadLine());
+            if ((n >= 1) && (n <= 100000))
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    List<string> arr = Console.ReadLine().TrimEnd().Split(' ').ToList();
+                    nameNum.Add(arr[0], arr[1]);
+                }
+                for (int j = 0; j < n; j++)
+                {
+                    string search = Console.ReadLine();
+
+                    if (nameNum.ContainsKey(search))
+                    {
+                        Console.WriteLine(search + "={0}", nameNum[search]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not found");
+                    }
+
+                }
+            }
+        }
+    }
+}
