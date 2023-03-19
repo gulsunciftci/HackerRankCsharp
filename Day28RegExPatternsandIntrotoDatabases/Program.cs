7bace46de456f9070b182343c88d5a5aea436d11
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-03-20 01:51:15 +0300

    HackerRank Day 28: RegEx, Patterns, and Intro to Databases problemi çözüldü

---
+++ Day28RegExPatternsandIntrotoDatabases/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day28RegExPatternsandIntrotoDatabases
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine().Trim());
+            List<string> list = new List<string>();
+            while (N-- > 0)
+            {
+                string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+                string firstName = firstMultipleInput[0];
+
+                string emailID = firstMultipleInput[1];
+
+                if (Regex.IsMatch(emailID, @".+@gmail\.com$"))
+                {
+                    list.Add(firstName);
+                }
+            }
+            list.Sort();
+            foreach (var n in list)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
